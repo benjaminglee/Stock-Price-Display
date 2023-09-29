b195f9f428cafe160da65554f6b45462de78ed79
Benjamin Lee <benjaminlee0727@gmail.com>
2023-09-29 16:53:45 -0700

    create global  style object

---
+++ src/components/StockStream/StockStream.tsx
@@ -0,0 +1,13 @@
+import StyledStockStream from "./StockStream.styled.js";
+
+const StockStream = () => {
+    return (
+    <StyledStockStream>
+        {/* <SearchBar />
+        <Graph />
+        <StockTable /> */}
+    </StyledStockStream>
+    )
+}
+
+export default StockStream;
---
+++ src/styles/global.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+body {
+    font-family: 'Poppins', sans-serif;
+    background-color: #1F2123;
+    * {
+        color: #F0F3EF
+    };
+}
+`;
